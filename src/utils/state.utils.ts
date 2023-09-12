7de9939da18ab209764a114c562cd09e1e7004c0
Romain Racamier-Lafon <romain.racamier@gmail.com>
2023-09-12 12:18:42 +0200

    cover state with unit tests

--- src/utils/state.utils.ts
+++ src/utils/state.utils.ts
@@ -1,49 +1,45 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-/* eslint-disable no-underscore-dangle */
 import { sleep } from 'shuutils'
 import { logger } from './logger.utils'
 
-class Machine {
+export class Machine {
+
+  private localState: 'initial' | 'loose' | 'pouring' | 'ready' | 'selected' | 'win' = 'initial'
+
+  private readonly localContext = { bottles: [], selected: -1 }
+
   public get state () {
-    return this._state
-  }
-  private set state (value: typeof this._state) {
-    this._state = value
-    logger.debug(`state changed to "${value}"`)
+    return this.localState
   }
   public get context () {
-    return this._context
+    return this.localContext
   }
-  private deselect () {
-    this._context.selected = -1
-  }
-  private needState (stateFrom: typeof this._state, stateTo: typeof this._state) {
-    if (stateFrom === this._state) return
-    throw new Error(`need to be in "${stateFrom}" state to go into "${stateTo}" but current state is "${this._state}"`)
+  private transition (from: typeof this.localState, to: typeof this.localState) {
+    logger.debug(`state transition ${from} => ${to} (actual ${this.localState})`)
+    if (from !== this.localState) throw new Error(`state cannot apply transition ${from} => ${to} (actual ${this.localState})`)
+    this.localState = to
   }
   public start () {
-    this.needState('initial', 'ready')
-    this.state = 'ready'
+    logger.debug('state start')
+    this.transition('initial', 'ready')
     // setup game
-    this.deselect()
   }
   public select (index: number) {
-    this.needState('ready', 'selected')
-    this.state = 'selected'
-    this._context.selected = index
+    logger.debug('state select')
+    this.transition('ready', 'selected')
+    this.localContext.selected = index
+  }
+  public deselect (from: typeof this.localState = 'selected') {
+    this.transition(from, 'ready')
+    this.localContext.selected = -1
   }
   public async pour (index: number) {
-    this.needState('selected', 'pouring')
-    this.state = 'pouring'
-    logger.info(`pouring bottle ${this._context.selected} into bottle ${index}`)
+    this.transition('selected', 'pouring')
+    logger.info(`pouring bottle ${this.localContext.selected} into bottle ${index}`)
     // pouring logic...
     // eslint-disable-next-line @typescript-eslint/no-magic-numbers
-    await sleep(2000)
-    this.deselect()
-    this.state = 'ready'
+    await sleep(600)
+    this.deselect('pouring')
   }
-  private _state: 'initial' | 'loose' | 'pouring' | 'ready' | 'selected' | 'win' = 'initial'
-  private readonly _context = { bottles: [], selected: -1 }
 }
 
 export const machine = new Machine()
---
+++ tests/state.test.ts
@@ -0,0 +1,44 @@
+import { sleep } from 'shuutils'
+import { expect, it } from 'vitest'
+import { Machine } from '../src/utils/state.utils'
+
+it('state A initial > ready > selected', () => {
+  const machine = new Machine()
+  expect(machine.state).toBe('initial')
+  machine.start()
+  expect(machine.state).toBe('ready')
+  machine.select(2)
+  expect(machine.state).toBe('selected')
+})
+
+
+it('state B initial cannot go into selected', () => {
+  const machine = new Machine()
+  expect(machine.state).toBe('initial')
+  expect(() => machine.select(2)).toThrowError()
+})
+
+it('state C selected > ready', () => {
+  const machine = new Machine()
+  machine.start()
+  machine.select(2)
+  machine.deselect()
+  expect(machine.state).toBe('ready')
+})
+
+it('state D selected > pouring > ready', async () => {
+  const machine = new Machine()
+  machine.start()
+  machine.select(2)
+  expect(machine.state).toBe('selected')
+  await sleep(1)
+  await machine.pour(3)
+  expect(machine.state).toBe('ready')
+})
+
+it('state E get context', () => {
+  const machine = new Machine()
+  machine.start()
+  machine.select(2)
+  expect(machine.context.selected).toBe(2)
+})
